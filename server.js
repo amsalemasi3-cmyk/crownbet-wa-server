d21829349eabd30509b67f7ec59ecb00a3f51f99
amsalemasi3-cmyk <amsalemasi3@gmail.com>
2026-05-08 19:04:58 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require('express'); 
 const cors = require('cors');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcode = require('qrcode');
